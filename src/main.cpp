18213393f40d86940f08e4237ddb3883af6c187c
Rob-NY <rrothe@mindspring.com>
2022-10-17 14:33:38 -0400

    jamulusserver/* RPC methods; connection notifications, chat notify, chat send broadcast/connection

--- src/main.cpp
+++ src/main.cpp
@@ -990,6 +990,7 @@ int main ( int argc, char** argv )
             if ( pRpcServer )
             {
                 new CServerRpc ( &Server, pRpcServer, pRpcServer );
+                CRpcLogging::getInstance().setRpcEnabled();
             }
 #endif
 
