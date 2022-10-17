f1f41a8fef66522a08e896698263c9fdb1eb35f1
Rob-NY <rrothe@mindspring.com>
2022-10-17 14:33:38 -0400

    jamulusserver/* RPC methods; connection notifications, chat notify, chat send broadcast/connection

--- src/server.h
+++ src/server.h
@@ -169,11 +169,16 @@ public:
     void SetEnableDelayPanning ( bool bDelayPanningOn ) { bDelayPan = bDelayPanningOn; }
     bool IsDelayPanningEnabled() { return bDelayPan; }
 
+    bool CreateAndSendPreEscapedChatText ( const int iChannel, const QString& strChatText );
+
+    // Methods formally protected, now public so they can be accessed via RPC
+    virtual void CreateAndSendChatTextForAllConChannels ( const int iCurChanID, const QString& strChatText );
+    int          FindChannel ( const CHostAddress& CheckAddr, const bool bAllowNew = false );
+
 protected:
     // access functions for actual channels
     bool IsConnected ( const int iChanNum ) { return vecChannels[iChanNum].IsConnected(); }
 
-    int                   FindChannel ( const CHostAddress& CheckAddr, const bool bAllowNew = false );
     void                  InitChannel ( const int iNewChanID, const CHostAddress& InetAddr );
     void                  FreeChannel ( const int iCurChanID );
     void                  DumpChannels ( const QString& title );
@@ -181,8 +186,6 @@ protected:
 
     virtual void CreateAndSendChanListForAllConChannels();
     virtual void CreateAndSendChanListForThisChan ( const int iCurChanID );
-
-    virtual void CreateAndSendChatTextForAllConChannels ( const int iCurChanID, const QString& strChatText );
 
     virtual void CreateOtherMuteStateChanged ( const int iCurChanID, const int iOtherChanID, const bool bIsMuted );
 
@@ -312,6 +315,7 @@ signals:
     void Started();
     void Stopped();
     void ClientDisconnected ( const int iChID );
+    void rpcClientDisconnected ( const int iChID, const QString strName, const CHostAddress HostAddress );
     void SvrRegStatusChanged();
     void AudioFrame ( const int              iChID,
                       const QString          stChName,
@@ -326,6 +330,8 @@ signals:
     void StopRecorder();
     void RecordingSessionStarted ( QString sessionDir );
     void EndRecorderThread();
+
+    void rpcChatSent ( const int iCurChanID, const QString chanName, const QString chanAddr, const QString chatStamp, const QString strChatMessage );
 
 public slots:
     void OnTimer();
